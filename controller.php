7d1e58383f4370987e676868ec10f4657eb74ead
Michele Locati <michele@locati.it>
2023-06-17 23:40:43 +0200

    Version 1.0.2

--- controller.php
+++ controller.php
@@ -18,7 +18,7 @@ class Controller extends Package implements ProviderInterface
      */
     protected $appVersionRequired = '8.5';
 
-    protected $pkgVersion = '1.0.1';
+    protected $pkgVersion = '1.0.2';
 
     /**
      * {@inheritdoc}
